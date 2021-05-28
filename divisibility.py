446c293baca42b1fad5a1201aadf345f73ec034e
Niveditha <nivi.krishna2000@gmail.com>
2021-05-28 19:05:25 +0530

    Initial version of program

---
+++ divisibility.py
@@ -0,0 +1,23 @@
+def digits(m,n,x,count):
+    
+     p = int(m)
+     q = int(n)
+     r = int(x)
+     
+     while p<=q :
+         if p%r == 0:
+             count += 1
+             
+         p += 1
+      
+     return(count)
+
+count = 0        
+a = input("No. of digits: ")
+x = input("divisibility rule of ")
+print("No. of "+a+" digit numbers divisible by "+x)
+m = input("from ")
+n = input("to ")
+print("is:")
+print(digits(m,n,x,count))
+ 
---
+++ prog1.py
@@ -0,0 +1,5 @@
+def length(name):
+     return len(str(name))
+   
+print(length("mike"))
+print("hello")
