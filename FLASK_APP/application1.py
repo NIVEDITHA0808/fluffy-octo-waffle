f8e75810b0dff9b7a0f4e81f22d6f60d054cd3b0
K V V S K NIVEDITHA <84661024+NIVEDITHA0808@users.noreply.github.com>
2021-07-31 10:44:13 +0530

    Create application1.py

---
+++ FLASK_APP/application1.py
@@ -0,0 +1,43 @@
+import csv
+from flask import Flask,render_template,request,redirect
+
+app = Flask(__name__)
+
+students=[]
+
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/register",methods=['POST'])
+def register():
+    
+    if not request.form.get("name") or not request.form.get('dorm') :
+       return render_template("failure.html") 
+    
+    file =open("registered.csv","a")
+    writer=csv.writer(file)
+    writer.writerow((request.form.get("name"),request.form.get('dorm')))
+    file.close()
+    
+    return render_template("success.html") 
+
+@app.route("/registered")
+def registered():
+    
+    with open("registered.csv","r") as file:
+         reader = csv.reader(file)
+         students = list(reader)
+    return render_template("registered.html",students=students) 
+  
+
+
+
+
+
+
+
+
+
+
+
